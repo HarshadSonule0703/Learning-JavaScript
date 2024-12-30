48c1a25d100f6db3bc4f8214152dd6b88c6efd84
HarshadSonule0703 <harshadsonule44@gmail.com>
2024-12-30 23:04:31 +0530

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+let a = "Harsh";
+let b = 10;
+let c = 1258848855737844833;
+let d = true;
+let e = undefined;
+let f = null;
+
+console.log(a, b, c, d, e, f);
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f);
+
+let o = {
+    name: "Harsh",
+    "job role":  "developer"
+}
+
+console.log(o);
+
+o.salary  = "100coror";
+console.log(o);
