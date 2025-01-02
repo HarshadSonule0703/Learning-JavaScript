efe5ad9f0897d08f408f332bb7b6ad275285ebe6
HarshadSonule0703 <harshadsonule44@gmail.com>
2025-01-02 23:45:37 +0530

    practice

---
+++ business_Name_Generator/factorial.js
@@ -0,0 +1,9 @@
+let num = prompt("Enter the number: ")
+
+let temp = 1;
+let fact;
+for(let i = num;i>0;i--){
+    temp = temp * i;
+
+}
+alert("The factorial is: "+ temp)
---
+++ business_Name_Generator/index.js
@@ -0,0 +1,77 @@
+/*
+Adjective :
+crazy
+amezing 
+fire
+
+Shop Name:
+Engine
+Food
+Garments
+
+Another word:
+Bros 
+limited
+Hub
+
+*/
+
+
+// let rand1 = Math.random();
+// let first;
+// let second, third;
+// if(rand1 < 0.33){
+//      first = "Crazy";
+
+// }
+// else if(rand1 >=0.33 && rand1 <0.66){
+//      first = "Amezing";
+// }
+// else{
+//      first = "Fire";
+// }
+
+
+// let rand2 = Math.random();
+// if(rand2 < 0.33){
+//      second = "egine";
+
+// }
+// else if(rand2 >=0.33 && rand2 <0.66){
+//      second = "food";
+// }
+// else{
+//      second = "garment";
+// }
+
+// let rand3 = Math.random();
+// if(rand3 < 0.33){
+//      third = "bros";
+
+// }
+// else if(rand3 >=0.33 && rand3 <0.66){
+//      third = "limited";
+// }
+// else{
+//      third = "hub";
+// }
+
+// // console.log(first +" " +second+ " "+ third);
+
+// console.log(`${first} ${second} ${third}`);
+
+
+// console.log("har\"".length);
+// console.log("harshad\"".length);
+
+// let MyName = "Harsh";
+// console.log(MyName.startsWith("H"));
+// console.log(MyName.endsWith("h"));
+// console.log(MyName.includes("sh"));
+
+// let MyName = "Haarshad";
+// let newName = MyName.toLowerCase();
+// console.log(newName);
+
+// let newString = "Pease give Rs.1000"
+// console.log(newString.slice(14,18))
